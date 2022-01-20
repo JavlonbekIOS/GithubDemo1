1f172e2abb24f5e261139062840962852512006d
Javlonbek <javlonbekalhamdulillah@gmail.com>
2022-01-20 13:29:44 +0500

    changed

--- GithubDemo1/controller/HomeViewController.swift
+++ GithubDemo1/controller/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  GithubDemo1
 //
 //  Created by Javlonbek on 20/01/22.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
 
-}
 
+}
